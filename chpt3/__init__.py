cf29c61ab56172d4180eae7f6c09c6cfe5dc7fc2
214233758 <30902957+214233758@users.noreply.github.com>
2019-10-24 20:15:18 +0200

    Update __init__.py

--- chpt3/__init__.py
+++ chpt3/__init__.py
@@ -7,11 +7,11 @@ def exists():
     check50.exists("question3.c")
     
 @check50.check(exists)
-def compiles()
+def compiles():
     """question3.c compiles"""
     check50.c.compiles(question3.c)
     
 @check50.check(compiles)
-def test()
+def test1():
     """expected output"""
     check50.run("./question3").stdout("First condition is true\nSecond condition is false\nThird condition is true", regex=True).exit(0)
