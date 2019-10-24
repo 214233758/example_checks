cb2055ad2f6c0f7b7d4622211616cc9b5767c8df
214233758 <30902957+214233758@users.noreply.github.com>
2019-10-24 22:25:26 +0200

    Update __init__.py

--- switch/__init__.py
+++ switch/__init__.py
@@ -14,24 +14,24 @@ def compiles():
 @check50.check(compiles)
 def test1():
     """ 1 outputs January"""
-    check50.run("./switch").stdin("1").stdout("January").stdout(check50.EOF).exit(0)
+    check50.run("./switch").stdin("1").stdout("January").exit(0)
 
 @check50.check(compiles)
 def test2():
     """ 6 outputs June"""
-    check50.run("./switch").stdin("6").stdout("June").stdout(check50.EOF).exit(0)
+    check50.run("./switch").stdin("6").stdout("June").exit(0)
     
 @check50.check(compiles)
 def test3():
     """ 12 outputs December"""
-    check50.run("./switch").stdin("12").stdout("December").stdout(check50.EOF).exit(0)
+    check50.run("./switch").stdin("12").stdout("December").exit(0)
     
 @check50.check(compiles)
 def test4():
     """ -1 outputs Error: no such month in my calendar"""
-    check50.run("./switch").stdin("-1").stdout("Error: no such month in my calendar").stdout(check50.EOF).exit(0)
+    check50.run("./switch").stdin("-1").stdout("Error: no such month in my calendar").exit(0)
     
 @check50.check(compiles)
 def test5():
     """ 13 outputs Error: no such month in my calendar"""
-    check50.run("./switch").stdin("13").stdout("Error: no such month in my calendar").stdout(check50.EOF).exit(0)
+    check50.run("./switch").stdin("13").stdout("Error: no such month in my calendar").exit(0)
