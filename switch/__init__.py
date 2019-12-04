f619c7824559643c061f7e76f5583beb9a83d4b6
214233758 <30902957+214233758@users.noreply.github.com>
2019-12-04 17:11:49 +0200

    Update __init__.py

--- switch/__init__.py
+++ switch/__init__.py
@@ -29,9 +29,9 @@ def test3():
 @check50.check(compiles)
 def test4():
     """ -1 outputs Error: no such month in my calendar"""
-    check50.run("./month").stdin("-1", prompt = False).stdout("Error: no such month in my calendar").exit(0)
+    check50.run("./month").stdin("-1", prompt = False).stdout("Error: no such month in my calend[ae]r").exit(0)
     
 @check50.check(compiles)
 def test5():
     """ 13 outputs Error: no such month in my calendar"""
-    check50.run("./month").stdin("13", prompt = False).stdout("Error: no such month in my calendar").exit(0)
+    check50.run("./month").stdin("13", prompt = False).stdout("Error: no such month in my calend[ae]r").exit(0)
